03a08d56cb2ff38131ed15e784870014f3a61989
yss <yss930819@163.com>
2020-12-15 13:58:49 +0800

    为 string number integer 和 boolean  增加默认值选项

--- packages/json-schema-editor/type/boolean.js
+++ packages/json-schema-editor/type/boolean.js
@@ -1,11 +1,15 @@
 const value = {
-    description: null
-}
+  description: null,
+};
 const attr = {
-    description: {
-      name: '描述',
-      type: 'string'
-    }
-}
-const wrapper = {value, attr}
-export default wrapper
+  description: {
+    name: "描述",
+    type: "string",
+  },
+  default: {
+    name: "默认值",
+    type: "boolean",
+  },
+};
+const wrapper = { value, attr };
+export default wrapper;
